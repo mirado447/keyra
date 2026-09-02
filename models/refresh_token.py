c62d4a713346b3980dce88746f636772c87a7daa
Mirado <hei.mirado@gmail.com>
2026-09-02 21:26:37 +0300

    feat: create modeles sqlalchemy

---
+++ models/refresh_token.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from datetime import datetime
+from database import Base
+
+class RefreshToken(Base):
+
+    __tablename__ = "refresh_tokens"
+
+    id = Column(Integer, primary_key=True, index=True)
+    token_hash = Column(String(255), unique=True, nullable=False)
+    create_at = Column(DateTime, default=database.utcnow)
+    revoked = Column(Boolean, default=False)
+    expire_at = Column(DateTime, default=datetime.utcnow)
+    id_user = Column(Integer, ForeignKey("endusers.id"), nullable=False)
