2e5831fe3e2110d3cbcbaf7d75ce2a65ba9ba2e3
Mirado <hei.mirado@gmail.com>
2026-09-07 12:02:22 +0300

    feat(auth): add JWT token creation and verification

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,17 @@
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
+import os
+
+load_dotenv()
 
 # Création d'un contexte de chiffrement/hachage des mots de passe avec bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 
 #Transforme un mot de passe en hash sécurisé
 def hash_password(password: str) -> str:
@@ -10,3 +20,16 @@ def hash_password(password: str) -> str:
 # Vérifie si un mot de passe correspond à un hash
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str) -> dict | None:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
