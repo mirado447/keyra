b9e9607c12219cbac97df0a82477661f9d62bb83
Mirado <hei.mirado@gmail.com>
2026-09-07 10:52:32 +0300

    docs: add comments to password hashing functions

--- app/core/security.py
+++ app/core/security.py
@@ -1,9 +1,12 @@
 from passlib.context import CryptContext
 
+# Création d'un contexte de chiffrement/hachage des mots de passe avec bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+#Transforme un mot de passe en hash sécurisé
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
+# Vérifie si un mot de passe correspond à un hash
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
