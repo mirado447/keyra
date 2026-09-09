b6b6be46835b63c32a2ec415660e96db9f8a76cd
Mirado <hei.mirado@gmail.com>
2026-09-09 16:02:52 +0300

    refactor: max_digits to max length

--- app/schemas/enduser.py
+++ app/schemas/enduser.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr, ConfigDict, Field
 from datetime import datetime
 
 class EndUserCreate(BaseModel):
-    name: str = Field(min_length=2, max_digits=100)
+    name: str = Field(min_length=2, max_length=100)
     email: EmailStr
     password: str = Field(min_length=8, max_length=72)
 
