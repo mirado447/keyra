7502658757b459d61d533aae6fb8419c887efff0
Mirado <hei.mirado@gmail.com>
2026-09-12 18:43:50 +0300

    fix url for CORS

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://keyra-production-8404.up.railway.app"],
+        "https://keyra-dashboard.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
